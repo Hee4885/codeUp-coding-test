9299f116d88705084f143cc1a6026b4b1dd9c5f9
Hee4885 <hijeon80@gmail.com>
2025-03-27 17:04:13 +0900

    commit message

---
+++ Java/1012_41908376(AC).java
@@ -0,0 +1,10 @@
+﻿import java.util.Scanner;
+
+public class Main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        double number = sc.nextDouble();
+        System.out.printf("%.6f",number);
+    }
+}
